72bf06dc428343a33311ac8f63de6e10e20035a7
Olivia Rochat <rochat.olivia@gmail.com>
2024-02-20 10:56:00 +0100

    Display all the tasks

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+
+  # As a user, I can list tasks
+  # As a user, I can view the details of a task
+  # As a user, I can add a new task
+  # As a user, I can edit a task (mark as completed / update title & details)
+  # As a user, I can remove a task
+
+  # resources :tasks
+  get "/", to: "tasks#index"
 end
