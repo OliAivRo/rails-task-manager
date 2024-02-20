2f812f0e970b672f124549fa2ce544fe416a7f30
Olivia Rochat <rochat.olivia@gmail.com>
2024-02-20 13:16:39 +0100

    As a user, I can edit a task

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,27 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
-
-
-  # As a user, I can list tasks
-  # As a user, I can view the details of a task
-  # As a user, I can add a new task
   # As a user, I can edit a task (mark as completed / update title & details)
   # As a user, I can remove a task
 
-  # resources :tasks
-  get "/", to: "tasks#index", as: :tasks
-  get "/task/:id", to: "tasks#show", as: :task
-  get "/new", to: "tasks#new"
-  post "/", to: "tasks#create"
+  # Read all
+  get "/tasks", to: "tasks#index"
+
+  # # Create
+  get "/tasks/new", to: "tasks#new", as: :new_task
+  post "/tasks", to: "tasks#create"
+
+  # Update
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+
+  # Read one
+  get "tasks/:id", to: "tasks#show", as: :task
+
+
+  # Delete
+  # delete "tasks/:id", to: "tasks#destroy"
+
+
+
 end
